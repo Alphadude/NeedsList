4b87dd26e8965bf7a434317799598c241e6e8d3c
jason ofua <jasonofua@gmail.com>
2018-02-15 21:19:48 +0100

    update the xml files

--- app/src/main/java/com/ng/needslist/needslist/Fragments/NeedsFragment.java
+++ app/src/main/java/com/ng/needslist/needslist/Fragments/NeedsFragment.java
@@ -1,19 +1,27 @@
 package com.ng.needslist.needslist.Fragments;
 
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ng.needslist.needslist.R;
+
+import static com.ng.needslist.needslist.R.drawable.ic_map;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NeedsFragment extends Fragment {
+
+    private ImageView dp;
+    private TextView user, message;
 
 
     public NeedsFragment() {
@@ -22,10 +30,13 @@ public class NeedsFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_needs, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_needs, container, false);
+        //return inflater.inflate(R.layout.fragment_tender_page, container, false);
+        dp = (ImageView)rootView.findViewById(R.id.userimage);
+        //dp.setImageResource();
+        return rootView;
     }
 
 }
--- app/src/main/java/com/ng/needslist/needslist/Fragments/SalesFragment.java
+++ app/src/main/java/com/ng/needslist/needslist/Fragments/SalesFragment.java
@@ -3,6 +3,7 @@ package com.ng.needslist.needslist.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,18 +14,20 @@ import com.ng.needslist.needslist.R;
  * A simple {@link Fragment} subclass.
  */
 public class SalesFragment extends Fragment {
+    private CardView sales;
 
 
     public SalesFragment() {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_sales, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sales, container, false);
+        //return inflater.inflate(R.layout.fragment_tender_page, container, false);
+        sales = (CardView)rootView.findViewById(R.id.card_view);
+        return rootView;
     }
 
 }
